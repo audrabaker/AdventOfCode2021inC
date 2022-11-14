83b02f692112bef1fd2cff3c37a0264ac30489b6
Audra Mitchell <aubaker@redhat.com>
2022-11-14 14:35:13 -0500

    Refactored code to be easier to read and have more than just a main function.

--- Day3-Part1.c
+++ Day3-Part1.c
@@ -20,34 +20,33 @@ char input[] = "Input-Day3.txt";
 //(input == "Test-Day3.txt") ? 5 : 12
 #define MAXSIZE 12
 FILE *fileptr;
+
 struct entry {
     char listelement[13];
     LIST_ENTRY(entry) entries;
 };
-LIST_HEAD(firstpasslisthead, entry) firstpass;
-LIST_HEAD(Oxlisthead, entry) OxRate;
-LIST_HEAD(COlisthead, entry) COScrub;
 
-int main(int argc, char *argv[]) {
+LIST_HEAD(entries, entry);
 
-    /* Open the file 
-    */
-	if ((fileptr = fopen(input, "r")) == NULL) {
-        printf("No file found");
-        exit(1);
-    }
+struct entries firstpass, OxRate, COScrub;
 
-    /* Process the file input and create a list (firstpass)
-     * of strings. While creating the list also count the 
-     * number of 1's seen in the high order bit.  
-     * The last variable is used to maintain the order of the
-     * entries encountered- otherwise the list would be backwards.
-    */
-    int total = 0, numberOfOnes = 0;
+// Sizes for the respective lists. 
+// Also counters for the number of times 1 is seen for each list.
+int totalNumOfEntries = 0, OxRatesize = 0, COScrubsize = 0, OxNumOfOnes = 0, CONumOfOnes = 0; 
+
+/* Process the file input and create a list (firstpass)
+ * of strings. While creating the list also count the 
+ * number of 1's seen in the high order bit.  
+ * The last variable is used to maintain the order of the
+ * entries encountered- otherwise the list would be backwards.
+*/
+int processFileIntoList() {
 	char value[MAXSIZE + 1] = "";
+
+    int numberOfOnes = 0;
     LIST_INIT(&firstpass);
     struct entry *last;
-
+    
     while (fscanf(fileptr, "%s\n", &value) != EOF) {
         if (LIST_EMPTY(&firstpass) != 0) {
             struct entry *newentry = malloc(sizeof(struct entry));
@@ -63,72 +62,64 @@ int main(int argc, char *argv[]) {
         if (value[0] == '1') {
             numberOfOnes++;
         }
-        total++;
+        totalNumOfEntries++;
     }
+    return numberOfOnes;
+}
 
-   
-    /* We have our initial list (firstpass), now lets separate out 
-     * the entries based on the most common bit seen (0's or 1's). 
-     * Entries with the most common high order bit will be put 
-     * in the OxRate list with the other entries being put in the 
-     * COScrub list.
-    */
-    rewind(fileptr);
-    LIST_INIT(&OxRate);
-    LIST_INIT(&COScrub);
-    int Oxsize = 0, COsize = 0, OxNumOfOnes = 0; 
-
+void processFirstList(int numberOfOnes) {
     while (LIST_EMPTY(&firstpass) == 0) {
         //1's are more common
         struct entry *tmp = malloc(sizeof(struct entry));
         tmp = LIST_FIRST(&firstpass);
         LIST_REMOVE(tmp, entries);
-        if (numberOfOnes >= total - numberOfOnes) {
+
+        if (numberOfOnes >= totalNumOfEntries - numberOfOnes) {
             if (tmp->listelement[0] == '1') {
                 LIST_INSERT_HEAD(&OxRate, tmp, entries);
                 if (tmp->listelement[1] == '1') {
                     OxNumOfOnes++;
                 }
-                Oxsize++;
+                OxRatesize++;
             } else {
                 LIST_INSERT_HEAD(&COScrub, tmp, entries);
-                COsize++;
+                COScrubsize++;
+                if (tmp->listelement[1] == '1') {
+                    CONumOfOnes++;
+                }
             }
         } else {
             if (tmp->listelement[0] == '1') {
                 LIST_INSERT_HEAD(&COScrub, tmp, entries);
-                COsize++;
+                COScrubsize++;
+                if (tmp->listelement[1] == '1') {
+                    CONumOfOnes++;
+                }
             } else {
                 LIST_INSERT_HEAD(&OxRate, tmp, entries);
                 if (tmp->listelement[1] == '1') {
                     OxNumOfOnes++;
                 }
-                Oxsize++;
+                OxRatesize++;
             }
         }
     }
+}
 
-
-
-    /* Our data has been parsed into two lists according to
-     * the most and least common high order bits. So now
-     * start processing the OxRate list. 
-    */
-    
-    int OxTotal = 0, OxNumOfOnesSecPass = 0;
-
+void processO2List() {
+    int OxNumOfOnesSecPass = 0;
     for (int i = 1; i < MAXSIZE - 1; i++) {
         struct entry *next = LIST_FIRST(&OxRate);
-        OxTotal = Oxsize;
+        int total = OxRatesize;
         do {
-            if (Oxsize == 1) {
+            if (OxRatesize == 1) {
                 break;
             }
             //1's are more common, so remove the 0s
-            if (OxNumOfOnes >= OxTotal - OxNumOfOnes) {
+            if (OxNumOfOnes >= total - OxNumOfOnes) {
                 if (next->listelement[i] == '0') {
                     LIST_REMOVE(next, entries);
-                    Oxsize--;
+                    OxRatesize--;
                 } else if (next->listelement[i+1] == '1') {
                     OxNumOfOnesSecPass++;
                 }
@@ -136,7 +127,7 @@ int main(int argc, char *argv[]) {
                 // 0's are more commmon so remove the 1s
                 if (next->listelement[i] == '1') {
                     LIST_REMOVE(next, entries);
-                    Oxsize--;
+                    OxRatesize--;
                 } else if (next->listelement[i+1] == '1') {
                     OxNumOfOnesSecPass++;
                 }     
@@ -146,27 +137,26 @@ int main(int argc, char *argv[]) {
         OxNumOfOnesSecPass = 0;
     }
 
+}
 
+void processCOList() {
 
-    /* We've processed the OxRate list, so now let's 
-     * process the CoScrub list.
-    */
-    int COTotal = 0, COOfOnesSecPass = 0, CONumofOnes = OxNumOfOnes;
-
+    int COOfOnesSecPass = 0;
+    
     for (int i = 1; i < MAXSIZE - 1; i++) {
         struct entry *next = LIST_FIRST(&COScrub);
-        COTotal = COsize;
+        int total = COScrubsize;
         do {
-            if (COsize == 1) {
+            if (COScrubsize == 1) {
                 break;
             }
 
             //1's are more common, so remove the 1s
-            if (CONumofOnes >= COTotal - CONumofOnes) {
+            if (CONumOfOnes >= total - CONumOfOnes) {
                 // if we have a 1, remove it
                 if (next->listelement[i] == '1') {
                     LIST_REMOVE(next, entries);
-                    COsize--;
+                    COScrubsize--;
                 // Since we did not remove the list element,
                 // check to see if the next bit has a 1.
                 } else if (next->listelement[i+1] == '1') {
@@ -176,46 +166,65 @@ int main(int argc, char *argv[]) {
                 // 0's are more commmon so remove the 0s
                 if (next->listelement[i] == '0') {
                     LIST_REMOVE(next, entries);
-                    COsize--;
+                    COScrubsize--;
                 } else if (next->listelement[i+1] == '1') {
                     COOfOnesSecPass++;
                 }     
             }
         } while(next = LIST_NEXT(next, entries));
-
-        struct entry *tmp = malloc(sizeof(struct entry));
-        printf("size is %d\n", COsize);
-        LIST_FOREACH(tmp, &COScrub, entries) {
-            printf("%s\n", tmp->listelement);
-        }
-        printf("\n\n\n");
-        CONumofOnes = COOfOnesSecPass;
+        CONumOfOnes = COOfOnesSecPass;
         COOfOnesSecPass = 0;
     }
+}
 
-    // Now we have both our values, convert the value from a
-    // string representing a binary value to a decimal number.
-
-    int CONumber = 0, OxRating = 0;
+int convertToBinary(struct entries *list) {
+    int decimalVal = 0;
     struct entry *tmp = malloc(sizeof(struct entry));
-    tmp = LIST_FIRST(&OxRate);
+    tmp = LIST_FIRST(list);
 
     for (int i = 0; i < MAXSIZE; i++) {
         if (tmp->listelement[i] == '1') {
-            OxRating += 1 << MAXSIZE - i - 1;
+            decimalVal += 1 << MAXSIZE - i - 1;
         }
     }
-    printf("Oxrate is %s, which is %d\n", tmp->listelement, OxRating);
-    
-    tmp = LIST_FIRST(&COScrub);
-    for (int i = 0; i < MAXSIZE; i++) {
-        if (tmp->listelement[i] == '1') {
-            CONumber += 1 << MAXSIZE - i - 1;
-        }
-    }
-    printf("COscrub is %s, which is %d\n", tmp->listelement, CONumber);
+    return decimalVal;
+}
 
-    printf("lifesupport rating: %d\n", OxRating * CONumber);
+int main(int argc, char *argv[]) {
+
+    /* Open the file 
+    */
+	if ((fileptr = fopen(input, "r")) == NULL) {
+        printf("No file found");
+        exit(1);
+    }
+
+    int NumberOfOnes = processFileIntoList();
+  
+    /* We have our initial list (firstpass), now lets separate out 
+     * the entries based on the most common bit seen (0's or 1's). 
+     * Entries with the most common high order bit will be put 
+     * in the OxRate list with the other entries being put in the 
+     * COScrub list.
+    */
+    rewind(fileptr);
+    LIST_INIT(&OxRate);
+    LIST_INIT(&COScrub);
+    processFirstList(NumberOfOnes);
+
+
+    /* Our data has been parsed into two lists according to
+     * the most and least common high order bits. So now
+     * start processing the OxRate list. 
+    */
+    processO2List();
+
+    /* We've processed the OxRate list, so now let's 
+     * process the CoScrub list.
+    */
+    processCOList();
+
+    printf("Lifesupport Rating: %d\n", convertToBinary(&OxRate) * convertToBinary(&COScrub));
 
 	fclose(fileptr);
 	return 0;
